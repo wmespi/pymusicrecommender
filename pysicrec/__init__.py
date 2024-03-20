56c4c37cc61ea26482855164039c7f579dd94fb5
William Espinoza <williamespinoza@Williams-Mac-Studio.local>
2024-03-19 23:35:57 -0400

    feat: udpated dependencies to use precommit and to work with AZ webscraping functions

---
+++ pysicrec/__init__.py
@@ -0,0 +1,33 @@
+# Scrapping
+SCRAPE_PROXY = 'socks5://127.0.0.1:9050'
+SCRAPE_RTD_MINIMUM = 0.1
+SCRAPE_RTD_MAXIMUM = 0.5
+SCRAPE_RETRIES_AMOUNT = 10
+SCRAPE_RTD_ERROR_MINIMUM = 0.5
+SCRAPE_RTD_ERROR_MAXIMUM = 1
+
+# String cleaning
+STR_CLEAN_TIMES = 3
+STR_CLEAN_DICT = {
+    '\n\n': '\n',
+    '\n\r\n': '\n',
+    '\r': '',
+    '\n': ', ',
+    '  ': ' ',
+    ' ,': ',',
+    ' .': '.',
+    ' :': ':',
+    ' !': '!',
+    ' ?': '?',
+    ',,': ',',
+    '..': '.',
+    '::': ':',
+    '!!': '!',
+    '??': '?',
+    '.,': '.',
+    '.:': '.',
+    ',.': ',',
+    ',:': ',',
+    ':,': ':',
+    ':.': ':'
+}
